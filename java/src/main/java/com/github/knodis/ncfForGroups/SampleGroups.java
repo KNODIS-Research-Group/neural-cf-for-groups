82f96fbb96e150789b1c410a05b79768370a2457
Fernando Ortega <fortegarequena@gmail.com>
2022-11-21 12:14:36 +0100

    Added new way to sample groups

--- java/src/main/java/com/github/knodis/ncfForGroups/SampleGroups.java
+++ java/src/main/java/com/github/knodis/ncfForGroups/SampleGroups.java
@@ -16,11 +16,13 @@ import java.util.*;
  */
 public class SampleGroups {
 
-    public final static String DB_NAME = "ml1m";
+    public final static String DB_NAME = "anime";
 
     public final static Long RANDOM_SEED = 43L;
 
-    private final static int NUM_SAMPLES = 50000;
+    private final static int NUM_RATINGS = 5; // minimum number of common ratings in a group
+
+    private final static int NUM_GROUPS = 10000; // number of groups
 
     private final static int[] GROUP_SIZES = {2, 3, 4, 5, 6, 7, 8, 9, 10};
 
@@ -38,56 +40,103 @@ public class SampleGroups {
             datamodel = BenchmarkDataModels.MyAnimeList();
         }
 
+        // Build groups of different sizes
         for (int groupSize : GROUP_SIZES) {
             System.out.println("\nGenerating groups of size " + groupSize);
 
+            // Build a list to sample test items that have been rated by more that groupSize users
+            List <Integer> testItemIndexes = new ArrayList<>();
+            for (int testItemIndex = 0; testItemIndex < datamodel.getNumberOfTestItems(); testItemIndex++) {
+                TestItem testItem = datamodel.getTestItem(testItemIndex);
+                if (testItem.getNumberOfTestRatings() >= groupSize) {
+                    testItemIndexes.add(testItemIndex);
+                }
+            }
+
+            // Create output file and its headers
             File file = new File("../data/" + DB_NAME + "/groups-" + groupSize + ".csv");
 
             File parent = file.getAbsoluteFile().getParentFile();
             parent.mkdirs();
 
-            String[] headers = new String[1 + 2 * groupSize];
-            headers[0] = "item";
+            String[] headers = new String[2 + 2 * groupSize];
+            headers[0] = "group";
+            headers[1] = "item";
             for (int i = 0; i < groupSize; i++) {
-                headers[1+2*i] = "user-" + (i+1);
-                headers[2+2*i] = "rating-" + (i+1);
+                headers[2+2*i] = "user-" + (i+1);
+                headers[3+2*i] = "rating-" + (i+1);
             }
 
             CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(file), CSVFormat.DEFAULT.withHeader(headers));
 
-            int samples = 0;
-            while (samples < NUM_SAMPLES) {
+            // Sample groups
+            int groups = 0;
+            while (groups < NUM_GROUPS) {
 
-                List<String> record = new ArrayList<>();
+                // get random items
+                Collections.shuffle(testItemIndexes, rand);
 
-                int testItemIndex = rand.nextInt(datamodel.getNumberOfTestItems());
-                TestItem testItem = datamodel.getTestItem(testItemIndex);
+                // find users that have rated all the items
+                Set<Integer> testUserIndexesSet = new HashSet<>();
 
-                if (testItem.getNumberOfTestRatings() >= groupSize) {
-                    record.add(Integer.toString(testItem.getItemIndex()));
+                for (int i = 0; i < NUM_RATINGS; i++) {
+                    int testItemIndex = testItemIndexes.get(i);
+                    TestItem testItem = datamodel.getTestItem(testItemIndex);
 
-                    List<Integer> indexes = new ArrayList<>();
-                    for (int i = 0; i < testItem.getNumberOfTestRatings(); i++) {
-                        indexes.add(i);
+                    // it is the first user: add all the items
+                    if (i == 0) {
+                        for (int pos = 0; pos < testItem.getNumberOfTestRatings(); pos++) {
+                            int testUserIndex = testItem.getTestUserAt(pos);
+                            testUserIndexesSet.add(testUserIndex);
+                        }
+
+                    // it is not the first user: retain common items
+                    } else {
+                        Set<Integer> ratedItemsSet = new HashSet<>();
+                        for (int pos = 0; pos < testItem.getNumberOfTestRatings(); pos++) {
+                            int testUserIndex = testItem.getTestUserAt(pos);
+                            ratedItemsSet.add(testUserIndex);
+                        }
+
+                        testUserIndexesSet.retainAll(ratedItemsSet);
                     }
-                    Collections.shuffle(indexes);
+                }
 
-                    for (int i = 0; i < groupSize; i++) {
-                        int pos = indexes.get(i);
-                        int testUserIndex = testItem.getTestUserAt(pos);
+                //System.out.println(testUserIndexesSet.size());
 
-                        TestUser testUser = datamodel.getTestUser(testUserIndex);
-                        record.add(Integer.toString(testUser.getUserIndex()));
+                // sample users
+                if (testUserIndexesSet.size() >= groupSize) {
 
-                        double rating = testItem.getTestRatingAt(pos);
-                        record.add(Double.toString(rating));
+                    List<Integer> testUserIndexesList = new ArrayList<>(testUserIndexesSet); // conversion required to shuffle
+                    Collections.shuffle(testUserIndexesList, rand);
+
+                    for (int i = 0; i < NUM_RATINGS; i++) {
+                        int testItemIndex = testItemIndexes.get(i);
+                        TestItem testItem = datamodel.getTestItem(testItemIndex);
+
+                        List<String> record = new ArrayList<>();
+
+                        record.add(Integer.toString(groups)); // group
+                        record.add(Integer.toString(testItem.getItemIndex())); // item
+
+                        for (int g = 0; g < groupSize; g++) {
+                            int testUserIndex = testUserIndexesList.get(g);
+                            TestUser testUser = datamodel.getTestUser(testUserIndex);
+
+                            record.add(Integer.toString(testUser.getUserIndex())); // user
+
+                            int pos = testUser.findTestItem(testItemIndex);
+                            double rating = testUser.getTestRatingAt(pos);
+                            record.add(Double.toString(rating)); // rating
+                        }
+
+                        csvPrinter.printRecord(record);
                     }
 
-                    csvPrinter.printRecord(record);
-                    samples++;
+                    groups++;
 
-                    if (samples % 100 == 0) System.out.print(".");
-                    if (samples % 1000 == 0) System.out.println(" " + samples + " samples");
+                    if (groups % 100 == 0) System.out.print(".");
+                    if (groups % 1000 == 0) System.out.println(" " + groups + " groups");
                 }
             }
 
